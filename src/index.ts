92964ffeda7136df71e67d635e67bd606283cadb
Rifat <rifat.islam173@northsouth.edu>
2024-05-31 00:22:36 +0600

    added new queries

--- src/index.ts
+++ src/index.ts
@@ -9,22 +9,19 @@ import { Employee } from "./entity/Employee";
 AppDataSource.initialize()
   .then(async () => {
     console.log("Connected");
-    await seed();
+    // await seed();
+    getAllEmployees().then((data) =>
+      console.log(JSON.stringify(data, null, 2))
+    );
   })
   .catch((error) => console.log(error));
 
 const seed = async () => {
-  // const emp = employeeRepository.create({
-  //   name: "Adam",
-  //   department: "SD",
-  // });
-  // await employeeRepository.save(emp);
-
-  const employee = await employeeRepository
-    .createQueryBuilder("employee")
-    .leftJoinAndSelect("employee.assets", "asset")
-    .getMany()
-  console.log("🚀 ~ seed ~ users:", employee)
+  const emp = employeeRepository.create({
+    name: "Adam",
+    department: "SD",
+  });
+  await employeeRepository.save(emp);
 };
 
 const getEmployeeById = async (id: number): Promise<Employee> => {
@@ -40,4 +37,15 @@ const getAssetById = async (id: number): Promise<Asset> => {
     },
     relations: ["employee"],
   });
+};
+
+const getAllEmployees = () => {
+  return employeeRepository
+    .createQueryBuilder("employee")
+    .leftJoinAndSelect("employee.assets", "asset")
+    .getMany();
+};
+
+const getAllAssets = async (): Promise<Asset[]> => {
+  return assetRepository.find();
 };
